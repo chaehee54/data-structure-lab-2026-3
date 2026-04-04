c6bba1a651bcea02836d2a94dbb173b875fd3559
chaehee54 <cogml9811@naver.com>
2026-04-04 16:28:31 +0900

    Add files via upload

---
+++ Lec04_Array_Class/Car_Class/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "Car.h"
+#include <Windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+
+    SportsCar sc;
+    sc.setTurbo(true);
+    sc.changeGear(3);
+    sc.speedUp();
+    sc.display();
+
+    return 0;
+}
